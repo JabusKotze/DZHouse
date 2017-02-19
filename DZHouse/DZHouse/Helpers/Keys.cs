7e7847925e4e9cdf538679ba8e32d8e3bcf4c37a
Jabus Kotze <jabus@javus.co.za>
2017-02-19 22:21:13 +0200

    Removed Keys

--- DZHouse/DZHouse/Helpers/Keys.cs
+++ DZHouse/DZHouse/Helpers/Keys.cs
@@ -8,18 +8,18 @@ namespace DZHouse.Helpers
 {
     public class Keys
     {
-        public static readonly string ApplicationURL = "http://getappmit.azurewebsites.net/";
-        public static readonly string ApplicationMobileService = "getappmit.azurewebsites.net";
+        public static readonly string ApplicationURL = "";
+        public static readonly string ApplicationMobileService = "";
         
 
-        public static readonly string Auth0ApplicationNameSpace = "appmit.auth0.com";
-        public static readonly string Auth0ApplicationNameClientId = "CJ4vsArSln8D5NgdZA0UDvPpDlUXHvGE";
+        public static readonly string Auth0ApplicationNameSpace = "AUTH DOMAIN";
+        public static readonly string Auth0ApplicationNameClientId = "API KEY";
         public static readonly string AuthConnectionDZHouse = "Appmit";
         public static readonly string AuthConnectionFacebook = "facebook";
-        public static readonly string Auth0ApplicationScheme = "https://appmit.auth0.com";
+        public static readonly string Auth0ApplicationScheme = "AUTH SCHEME";
 
-        public static readonly string Email = "jabus@javus.co.za";
-        public static readonly string Password = "Ronney1990";
+        public static readonly string Email = "EMAIL";
+        public static readonly string Password = "Password";
 
     }
 }
